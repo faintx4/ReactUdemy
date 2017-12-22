b2877e4040c5eef761951e8fb5b1784c4d2fea92
sblashkin <faintx4@gmail.com>
2017-12-22 18:08:21 +0200

    styling for Person cards

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
-      <h1>My name is {props.name} and I am  {props.age} years old!</h1>
+    <div className="person-card">
+      <h1 className="person-card__name">My name is {props.name}</h1>
+      <h2 className="person-card__age">I am  {props.age} years old!</h2>
       <p>{props.children}</p>
     </div>
 
